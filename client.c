ba48c1e86cc087b91ed5c5b9ea20beb5187461d7
tlemesle <theo.lemesle42@gmail.com>
2021-07-27 16:24:16 +0200

    fix

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 15:12:01 by tlemesle          #+#    #+#             */
-/*   Updated: 2021/07/27 16:21:13 by tlemesle         ###   ########.fr       */
+/*   Updated: 2021/07/27 16:23:58 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int check_parameters(int ac, char **av)
 {
-    int i;
+	int i;
 
-    i = -1;
-    while (av[1][i++])
-        if (av[1][i] < 0 && av[1][i] > 9)
-            return (0);
-    if (ac != 3)
-        return (0);
-    return (1);
+	i = -1;
+	if (ac != 3)
+		return (0);
+	while (av[1][i++])
+		if (av[1][i] < 0 && av[1][i] > 9)
+			return (0);
+	return (1);
 }
 
 int main(int ac, char **av)
 {
-    if (check_parameters(ac, av) == 0)
-    {
-        printf("Error detected in program arguments !\n");
-        exit(0);
-    }
+	if (check_parameters(ac, av) == 0)
+	{
+		printf("Error detected in program arguments !\n");
+		exit(0);
+	}
 }
