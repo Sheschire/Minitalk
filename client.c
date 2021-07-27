4b62ab533b1373562d58c0fc8e3e4bcd9d82c7a6
tlemesle <theo.lemesle42@gmail.com>
2021-07-27 16:21:26 +0200

    blabla

--- client.c
+++ client.c
@@ -1,25 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/27 13:52:18 by tlemesle          #+#    #+#             */
-/*   Updated: 2021/07/27 16:17:49 by tlemesle         ###   ########.fr       */
+/*   Created: 2021/07/27 15:12:01 by tlemesle          #+#    #+#             */
+/*   Updated: 2021/07/27 16:21:13 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int		main(int ac, char **av)
+int check_parameters(int ac, char **av)
 {
-	(void)av;
-	printf("PID : %d\n", getpid());
-	if (ac != 1)
-	{
-		printf("Too much arguments !\n");
-		exit(0);
-	}
-	return (0);
+    int i;
+
+    i = -1;
+    while (av[1][i++])
+        if (av[1][i] < 0 && av[1][i] > 9)
+            return (0);
+    if (ac != 3)
+        return (0);
+    return (1);
+}
+
+int main(int ac, char **av)
+{
+    if (check_parameters(ac, av) == 0)
+    {
+        printf("Error detected in program arguments !\n");
+        exit(0);
+    }
 }
